86784e4d6e5ddf79d3a3cdb5612741fabfc670b8
Rezatama <reza240695@gmail.com>
2016-03-25 09:33:54 +0700

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,10 +1,13 @@
+#load dplyr library
 library(dplyr)
-
+#read dataset
 power <- read.csv("household_power_consumption.txt", sep=";", header=TRUE,na.strings="?")
-
+#filtering dataset
 power_sort <- filter(power,Date %in% c("1/2/2007","2/2/2007")) 
+#create new Date_time column as Date and Time
 powsort$Date_time<-strptime(paste(powsort$Date,powsort$Time),"%d/%m/%Y %H:%M:%S")
-
+#create png file
 png("plot2.png",width=480,height=480)
+#create plot
 plot(powsort$Date_time,powsort$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab="")
 dev.off()
