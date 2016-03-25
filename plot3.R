ca2a6a8bd0f707b62a077055a6532c7cb19db372
Rezatama <reza240695@gmail.com>
2016-03-25 09:36:01 +0700

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,11 +1,14 @@
+#load dplyr library
 library(dplyr)
-
+#read data set
 power <- read.csv("household_power_consumption.txt", sep=";", header=TRUE,na.strings="?")
-
+#filtering data set
 power_sort <- filter(power,Date %in% c("1/2/2007","2/2/2007")) 
+#create new Date_Time column as Date and Time
 powsort$Date_time<-strptime(paste(powsort$Date,powsort$Time),"%d/%m/%Y %H:%M:%S")
-
+#create png file
 png("plot3.png",width=480,height=480)
+#create plot
 plot(powsort$Date_time,powsort$Sub_metering_1,type="l",ylab = "Energy sub metering",xlab="")
 lines(powsort$Date_time,powsort$Sub_metering_2,col="red")
 lines(powsort$Date_time,powsort$Sub_metering_3,col="blue")
